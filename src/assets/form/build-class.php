1955288493ebcd01b6e42f61774eebe92656e1cb
Robert Laws <roblaws@me.com>
2019-04-01 11:44:22 +0300

    update class form and data file with email field

--- src/assets/form/build-class.php
+++ src/assets/form/build-class.php
@@ -1,5 +1,6 @@
 <?php
     $name = isset($_POST['name']) ? $_POST['name'] : null;
+    $email = isset($_POST['email']) ? $_POST['email'] : null;
     $course = isset($_POST['course']) ? $_POST['course'] : null;
     $date = isset($_POST['date']) ? $_POST['date'] : null;
     $time = isset($_POST['time']) ? $_POST['time'] : null;
@@ -24,7 +25,7 @@
 
     $next_id = count($array_data["classes"]) + 1;
 
-    $extra = array('id' => $next_id, 'instructor' => $name, 'course_title' => $course, 'date' => $date, 'time' => $time, 'students' => $students, 'instruction' => $instructionArr);  
+    $extra = array('id' => $next_id, 'instructor' => $name, 'email' => $email, 'course_title' => $course, 'date' => $date, 'time' => $time, 'students' => $students, 'instruction' => $instructionArr);  
     array_push($array_data["classes"], $extra);
     $final_data = json_encode($array_data);
     file_put_contents($myFile, $final_data);
